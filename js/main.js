ed585ac16b0b233ec26b6ad8ee3c6efc1b70f7f7
joelnzl <joelnlopez13@gmail.com>
2022-08-18 21:02:30 -0300

    bucle

---
+++ js/main.js
@@ -0,0 +1,14 @@
+console.log("desafio1");
+
+
+    const numero = Number(prompt("ingresar numero"));
+    
+    for (let i = 0; i <= 10; i++) {
+      console.log(numero + i);
+      if (i == 10) {
+        alert("hasta acá");
+      }
+     
+    }
+  
+  
